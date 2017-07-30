d77ed0da6bf11259cf21f272cfa5561e106e1603
Shivsunder Majhi <shivsunder.majhi19@gmail.com>
2017-07-31 02:07:25 +1200

    Models
    Containing the models of my website.

---
+++ MyWebsite/Models/Index.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyWebsite.Models
+{
+    public class Index
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
